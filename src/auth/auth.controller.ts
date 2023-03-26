463d0af74de6eeaf0caaca195d3231306ba0f297
unknown <boronilov@gmail.com>
2023-03-26 21:01:34 +0200

    prisma connecte to Postgres

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post, Body } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { Request } from "express";
+
+@Controller("auth")
+export class AuthController {
+    constructor (private authService: AuthService) {}
+
+    @Post("signup")
+    signup(@Body() dto: any){
+        return this.authService.signup();
+    }
+
+    @Post("signin")
+    signin(){
+        return this.authService.signin();
+    }
+}
