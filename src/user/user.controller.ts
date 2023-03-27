f66228b940e010ccb4fa1dea4b03a9e16a203292
unknown <boronilov@gmail.com>
2023-03-27 12:14:59 +0200

    custom param decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, UseGuards, Req } from '@nestjs/common';
-import { AuthGuard } from "@nestjs/passport"
-import { Request } from 'express';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-    @UseGuards(JwtGuard)
     @Get("me")
-    getMe(@Req() req: Request) {
-        return req.user;
+    getMe(@GetUser() user: User) {
+        return user;
     }
 }
