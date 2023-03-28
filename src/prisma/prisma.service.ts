c92f7557e2e5b220e8fe63c6e19ac56be9d9fa11
= <=>
2023-03-28 12:32:37 +0200

    test DB init

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,13 +4,19 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor(config: ConfigService) {
-        super({
-            datasources: {
-                db: {
-                    url: config.get("DATABASE_URL")
-                }
+   constructor(config: ConfigService) {
+      super({
+         datasources: {
+            db: {
+               url: config.get("DATABASE_URL"),
             }
-        })
-    }
+         }
+      })
+   }
+   cleanDb() {
+      return this.$transaction([
+         this.bookmark.deleteMany(),
+         this.user.deleteMany(),
+      ])
+   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,52 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { PrismaService } from '../src/prisma/prisma.service';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe("App e2e", () => {
+   let app: INestApplication;
+   let prisma: PrismaService;
+   beforeAll(async () => {
+      const moduleRef = await Test.createTestingModule({
+         imports: [AppModule]
+      }).compile();
+      app = moduleRef.createNestApplication();
+      app.useGlobalPipes(new ValidationPipe({
+         whitelist: true,
+       }));
+       await app.init();
+       prisma = app.get(PrismaService);
+       await prisma.cleanDb();
+   });
+   afterAll(() => {
+      app.close();
+   })
+   
+   describe("Auth", () => {
+      describe("Register", () => {
+         it.todo("should register");
+      });
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+      describe("Login", () => {
+         it.todo("should login");
+      });
+   })
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+   describe("User", () => {
+      describe("Get current user", () => {});
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
-});
+      describe("Edit user", () => {});
+   })
+
+   describe("Bookmark", () => {
+      describe("Get bookmarks", () => {});
+
+      describe("Create bookmark", () => {});
+
+      describe("Create bookmark by id", () => {});
+
+      describe("Edit bookmark", () => {});
+
+      describe("Delete bookmark", () => {});
+   })
+})
