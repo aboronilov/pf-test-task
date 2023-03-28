c3464341f0d98e9976656f9bb205925554e84274
= <=>
2023-03-28 17:54:45 +0200

    Auth e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,9 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
+import * as pactum from "pactum";
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { PrismaService } from '../src/prisma/prisma.service';
+import { AuthDto } from 'src/auth/dto';
 
 describe("App e2e", () => {
    let app: INestApplication;
@@ -13,40 +15,127 @@ describe("App e2e", () => {
       app = moduleRef.createNestApplication();
       app.useGlobalPipes(new ValidationPipe({
          whitelist: true,
-       }));
-       await app.init();
-       prisma = app.get(PrismaService);
-       await prisma.cleanDb();
+      }));
+      await app.init();
+      await app.listen(3333)
+      prisma = app.get(PrismaService);
+      await prisma.cleanDb();
+      pactum.request.setBaseUrl("http://localhost:3333")
    });
    afterAll(() => {
       app.close();
    })
-   
+
    describe("Auth", () => {
-      describe("Register", () => {
-         it.todo("should register");
+      const body: AuthDto = {
+         email: "johndoe@gmail.com",
+         password: "secret"
+      };
+
+      describe("Signup", () => {
+         it("should fail signup without email", () => {
+            return pactum
+               .spec()
+               .post("/auth/signup")
+               .withBody({ password: body.password })
+               .expectStatus(400)
+         })
+         it("should fail signup with incorrect email", () => {
+            return pactum
+               .spec()
+               .post("/auth/signup")
+               .withBody({
+                  email: "123",
+                  password: body.password
+               })
+               .expectStatus(400)
+         })
+         it("should fail signup without password", () => {
+            return pactum
+               .spec()
+               .post("/auth/signup")
+               .withBody({ email: body.email })
+               .expectStatus(400)
+         })
+         it("should fail signup without email and password", () => {
+            return pactum
+               .spec()
+               .post("/auth/signup")
+               .expectStatus(400)
+         })
+         it("should signup", () => {
+            return pactum
+               .spec()
+               .post("/auth/signup")
+               .withBody(body)
+               .expectStatus(201)
+         });
       });
 
       describe("Login", () => {
-         it.todo("should login");
+         it("should fail signin without email", () => {
+            return pactum
+               .spec()
+               .post("/auth/signin")
+               .withBody({ password: body.password })
+               .expectStatus(400)
+         })
+         it("should fail signin without password", () => {
+            return pactum
+               .spec()
+               .post("/auth/signin")
+               .withBody({ email: body.email })
+               .expectStatus(400)
+         })
+         it("should fail signin without email and password", () => {
+            return pactum
+               .spec()
+               .post("/auth/signin")
+               .expectStatus(400)
+         })
+         it("should signin", () => {
+            return pactum
+               .spec()
+               .post("/auth/signin")
+               .withBody(body)
+               .expectStatus(200)
+         });
+         it("should login", () => {
+            return pactum
+               .spec()
+               .post("/auth/signin")
+               .withBody(body)
+               .expectStatus(200)
+               .stores("userToken", "access_token")
+         });
       });
    })
 
    describe("User", () => {
-      describe("Get current user", () => {});
+      describe("Get current user", () => { 
+         it("should get current user", () => {
+            return pactum
+            .spec()
+            .withHeaders({
+               Authorization: `Bearer $S{userToken}`
+            })
+            .get("/users/me")
+            .expectStatus(200)
+         })
+       });
 
-      describe("Edit user", () => {});
+      describe("Edit user", () => { });
    })
 
    describe("Bookmark", () => {
-      describe("Get bookmarks", () => {});
+      describe("Get bookmarks", () => { });
 
-      describe("Create bookmark", () => {});
+      describe("Create bookmark", () => { });
 
-      describe("Create bookmark by id", () => {});
+      describe("Create bookmark by id", () => { });
 
-      describe("Edit bookmark", () => {});
+      describe("Edit bookmark by id", () => { });
 
-      describe("Delete bookmark", () => {});
+      describe("Delete bookmark by id", () => { });
    })
 })
